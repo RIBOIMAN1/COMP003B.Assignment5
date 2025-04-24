fc21e1b91b44446ddcfa2df3744ded843ede21c1
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-04-24 15:56:44 -0700

    Added a list of seven different games with real-life prices as different ID numbers in the file "GameStore.cs."

--- COMP003B.Assignment5/Data/GameStore.cs
+++ COMP003B.Assignment5/Data/GameStore.cs
@@ -1,6 +1,18 @@
-﻿namespace COMP003B.Assignment5.Data
+﻿using COMP003.Assignment5.Models;
+using COMP003B.Assignment5.Models;
+
+namespace COMP003.Assignment5.Data;
+
+public static class GameStore
 {
-	public class GameStore
+	public static List<Game> Games { get; } = new()
 	{
-	}
+		new Game { Id = 1, Name = "Tekken 8", Price = 59.99m },
+		new Game { Id = 2, Name = "The Legend of Zelda: Breath of the Wild", Price = 59.99m },
+		new Game { Id = 3, Name = "The Legend of Zelda: Breath of the Wild - Nintendo Switch 2 Edition", Price = 69.99m },
+		new Game { Id = 4, Name = "The Legend of Zelda: Tears of the Kingdom", Price = 69.99m },
+		new Game { Id = 5, Name = "The Legend of Zelda: Tears of the Kingdom - Nintendo Switch 2 Edition", Price = 79.99m },
+		new Game { Id = 6, Name = "Elden Ring", Price = 59.99m },
+		new Game { Id = 7, Name = "Super Smash Bros. Ultimate", Price = 59.99m },
+	};
 }
